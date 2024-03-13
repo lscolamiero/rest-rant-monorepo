b53bc96744bb4a8ca8daddd58fe84dddc228bf6f
lscolamiero <lautaro.scolamiero@gmail.com>
2024-03-12 19:30:40 -0700

    updated

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -11,11 +12,24 @@ router.post('/', async (req, res) => {
     })
 
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({
-            message: `Could not find a user with the provided username and password`
-        })
+        res.status(404).json({ message: `Could not find a user with the provided username and password` })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({ user: user, token: result.value })
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    try {
+        // let user = await User.findOne({
+        //     where: {
+        //         userId: 
+        //     }
+        // })
+        // res.json(user)
+    } catch (error) {
+        console.log(error)
+        res.json(null)
     }
 })
 
